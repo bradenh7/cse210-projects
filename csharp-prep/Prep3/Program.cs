4e9623c87116167b52fa599368c0a3a7ffcaa1b6
bradenh7 <bradenhall7@gmail.com>
2025-04-28 09:39:10 -0600

    Added a number guessing program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); 
+
+        int guess = -1;
+        while (true)
+        {
+            Console.WriteLine("What is your guess?");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
     }
 }
