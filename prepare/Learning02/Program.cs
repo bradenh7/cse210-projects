41f2e82915ab1dcb173df22bec69f32fbf615a9e
bradenh7 <bradenhall7@gmail.com>
2025-05-06 12:36:52 -0600

    Created the job program including resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Tech Corp";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+        
+
+        Job job2 = new Job();
+        job2._company = "Web Solutions";
+        job2._jobTitle = "Web Developer";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+        
+
+        Resume Resume1 = new Resume();
+        Resume1._name = "Jimmy Firecracker";
+        Resume1._jobs.Add(job1);
+        Resume1._jobs.Add(job2);
+        Resume1.Display();
+
     }
 }
