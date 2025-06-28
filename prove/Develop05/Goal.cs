03b5dedcbc04a92650dded9fda55a69ac202fd0f
bradenh7 <bradenhall7@gmail.com>
2025-06-28 16:34:06 -0600

    separated classes in files

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -19,30 +19,3 @@ abstract class Goal
     public abstract string Serialize();
 }
 
-// SimpleGoal.cs
-class SimpleGoal : Goal
-{
-    private bool _isComplete;
-
-    public override bool IsComplete => _isComplete;
-
-    public SimpleGoal(string name, string description, int points, bool isComplete = false)
-        : base(name, description, points)
-    {
-        _isComplete = isComplete;
-    }
-
-    public override int RecordEvent()
-    {
-        if (!_isComplete)
-        {
-            _isComplete = true;
-            return Points;
-        }
-        return 0;
-    }
-
-    public override string GetStatus() => IsComplete ? "[X]" : "[ ]";
-
-    public override string Serialize() => $"SimpleGoal|{Name}|{Description}|{Points}|{_isComplete}";
-}
---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,27 @@
+// SimpleGoal.cs
+class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public override bool IsComplete => _isComplete;
+
+    public SimpleGoal(string name, string description, int points, bool isComplete = false)
+        : base(name, description, points)
+    {
+        _isComplete = isComplete;
+    }
+
+    public override int RecordEvent()
+    {
+        if (!_isComplete)
+        {
+            _isComplete = true;
+            return Points;
+        }
+        return 0;
+    }
+
+    public override string GetStatus() => IsComplete ? "[X]" : "[ ]";
+
+    public override string Serialize() => $"SimpleGoal|{Name}|{Description}|{Points}|{_isComplete}";
+}
