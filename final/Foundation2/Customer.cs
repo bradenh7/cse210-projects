7a94d33723ccd69ebfe3eeaf9c41755258c9717c
bradenh7 <bradenhall7@gmail.com>
2025-07-05 16:47:34 -0600

    added le comments

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -2,30 +2,31 @@ using System;
 
 public class Customer
 {
-    private string _name;
-    private Address _address;
+    private string _name; // Customer's name
+    private Address _address; // Customer's address
 
+    // Properties to get and set the customer's name and address
     public string Name
     {
-        get {return _name;}
-        set{_name = value;}
+        get { return _name; }
+        set { _name = value; }
     }
 
     public Address Address
     {
-        get {return _address;}
-        set{_address = value;}
+        get { return _address; }
+        set { _address = value; }
     }
 
 
-
+    // Constructor to initialize the customer with a name and address
     public Customer(string name, Address address)
     {
         _name = name;
         _address = address;
     }
 
-
+    // Method to check if the customer is in the USA
     public bool InUSA()
     {
         return _address.InUSA();
