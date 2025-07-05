eac8296a4c75454f4d95f19530b1756f7ebf0262
bradenh7 <bradenhall7@gmail.com>
2025-07-05 16:48:39 -0600

    producto commento

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -2,15 +2,16 @@ using System;
 
 public class Product
 {
-    private string _name;
-    private int _productId;
-    private double _price;
-    private int _quantity;
+    private string _name; // Product name
+    private int _productId; // Product ID
+    private double _price; // Product price
+    private int _quantity; // Quantity of the product
 
+    // Properties to get and set the product details
     public string Name
     {
-        get {return _name;}
-        set{_name = value;}
+        get { return _name; }
+        set { _name = value; }
     }
 
     public int ProductId
@@ -25,6 +26,7 @@ public class Product
     }
 
 
+    // Constructor to initialize the product with name, ID, price, and quantity
     public Product(string name, int productId, double price, int quantity)
     {
         _name = name;
