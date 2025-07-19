dfa14d8bb04d49fc9db66501675486e60575c564
bradenh7 <bradenhall7@gmail.com>
2025-07-19 14:55:19 -0600

    comments

--- final/Foundation4/Running.cs
+++ final/Foundation4/Running.cs
@@ -1,12 +1,15 @@
 public class Running : Activity
 {
+    // attribute for Running class
     private double distance;
 
+    // constructor for Running class
     public Running(string date, int minutes, double distance) : base(date, minutes)
     {
         this.distance = distance;
     }
 
+    // override methods for Running class
     public override double GetDistance() => distance;
 
     public override double GetSpeed() => (distance / GetMinutes()) * 60;
