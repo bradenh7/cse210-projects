8e7120c4492f4b3a89262c6a2a743dfaa1bc700a
bradenh7 <bradenhall7@gmail.com>
2025-07-19 14:53:00 -0600

    foundation 4

---
+++ final/Foundation4/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 3.0),
+            new Cycling("03 Nov 2022", 45, 15.0),
+            new Swimming("03 Nov 2022", 30, 40)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
